f4abfbcd2f4062a302c33458cf39dca27f250c89
Amir Farzamnia <amirfarzamnia@proton.me>
2025-12-15 01:37:52 +0330

    fix: fix some type problems

--- src/types.ts
+++ src/types.ts
@@ -7,27 +7,49 @@ import {
   ZodRecord,
   ZodTuple,
   ZodLazy,
+  ZodOptional,
+  ZodNullable,
+  ZodDefault,
 } from "zod";
 
 export type DeepPartial<T extends z.core.SomeType> =
-  T extends ZodObject<infer Shape>
-    ? ZodObject<{ [k in keyof Shape]: DeepPartial<Shape[k]> }>
-    : T extends ZodArray<infer Type>
-      ? ZodArray<DeepPartial<Type>>
-      : T extends ZodUnion<infer Options>
-        ? ZodUnion<{ [k in keyof Options]: DeepPartial<Options[k]> }>
-        : T extends ZodIntersection<infer Left, infer Right>
-          ? ZodIntersection<DeepPartial<Left>, DeepPartial<Right>>
-          : T extends ZodRecord<infer Key, infer Value>
-            ? ZodRecord<Key, DeepPartial<Value>>
-            : T extends ZodTuple<infer Items>
-              ? ZodTuple<
-                  { [k in keyof Items]: DeepPartial<Items[k]> } extends infer U
-                    ? U extends any[]
-                      ? U
-                      : never
-                    : never
-                >
-              : T extends ZodLazy<infer Type>
-                ? ZodLazy<DeepPartial<Type>>
-                : T;
+  // Optional
+  T extends ZodOptional<infer Inner>
+    ? ZodOptional<DeepPartial<Inner>>
+    : // Nullable
+      T extends ZodNullable<infer Inner>
+      ? ZodNullable<DeepPartial<Inner>>
+      : // Default
+        T extends ZodDefault<infer Inner>
+        ? ZodDefault<DeepPartial<Inner>>
+        : // Objects
+          T extends ZodObject<infer Shape>
+          ? ZodObject<{ [K in keyof Shape]: DeepPartial<Shape[K]> }>
+          : // Arrays
+            T extends ZodArray<infer Type>
+            ? ZodArray<DeepPartial<Type>>
+            : // Unions
+              T extends ZodUnion<infer Options>
+              ? ZodUnion<{ [K in keyof Options]: DeepPartial<Options[K]> }>
+              : // Intersections
+                T extends ZodIntersection<infer Left, infer Right>
+                ? ZodIntersection<DeepPartial<Left>, DeepPartial<Right>>
+                : // Records
+                  T extends ZodRecord<infer Key, infer Value>
+                  ? ZodRecord<Key, DeepPartial<Value>>
+                  : // Tuples
+                    T extends ZodTuple<infer Items>
+                    ? ZodTuple<
+                        {
+                          [K in keyof Items]: DeepPartial<Items[K]>;
+                        } extends infer U
+                          ? U extends any[]
+                            ? U
+                            : never
+                          : never
+                      >
+                    : // Lazy / recursive
+                      T extends ZodLazy<infer Type>
+                      ? ZodLazy<DeepPartial<Type>>
+                      : // Fallback
+                        T;
